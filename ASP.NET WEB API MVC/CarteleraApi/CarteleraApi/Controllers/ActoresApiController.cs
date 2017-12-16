dad1ac68a92e75eee3e34079356fb42b31d806a8
fercin <fermincito2@hotmail.com>
2017-12-16 17:55:27 -0400

    WEB API MVC

---
+++ ASP.NET WEB API MVC/CarteleraApi/CarteleraApi/Controllers/ActoresApiController.cs
@@ -0,0 +1,112 @@
+﻿using CarteleraApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CarteleraApi.Controllers
+{
+    public class ActoresApiController : ApiController
+    {
+        private CarteleraEntities3 _db = new CarteleraEntities3();
+
+
+
+
+        public HttpResponseMessage Get(int id)
+        {
+            var actor = _db.Actores.FirstOrDefault(x => x.id == id);
+
+            return Request.CreateResponse<Actores>(HttpStatusCode.OK, actor);
+
+        }
+
+        public HttpResponseMessage Get()
+        {
+
+            var actores = _db.Actores;
+  
+            return Request.CreateResponse<IEnumerable<Actores>>(HttpStatusCode.OK, actores);
+
+        }
+
+        public HttpResponseMessage Post([FromBody]Actores newactor)
+        {
+         
+            var actor = _db.Actores.FirstOrDefault(x => x.nombre == newactor.nombre && x.apellido == newactor.apellido);
+
+            if (actor == null)
+            {
+                _db.Actores.Add(newactor);
+                _db.SaveChanges();
+
+                return new HttpResponseMessage(HttpStatusCode.Created);
+
+            }
+            else{
+                
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+            
+           
+
+           
+        }
+
+
+        public HttpResponseMessage Delete(int id)
+        {
+
+            var peliactor = _db.PeliculaActores.Where(x => x.idactor == id);
+            var deleteactor = _db.Actores.FirstOrDefault(x => x.id == id);
+
+            if (peliactor != null)
+            {
+                _db.PeliculaActores.RemoveRange(peliactor);
+                _db.SaveChanges();
+            }
+
+
+
+            
+            if (deleteactor != null)
+            {
+                _db.Actores.Remove(deleteactor);
+                _db.SaveChanges();
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+        }
+
+        public HttpResponseMessage Put([FromBody]Actores oldactor)
+        {
+
+            var putactor = _db.Actores.FirstOrDefault(x => x.id == oldactor.id);
+
+            if (putactor != null)
+            {
+                putactor.nombre = oldactor.nombre;
+                putactor.apellido= oldactor.apellido;
+                putactor.sexo = oldactor.sexo   ;
+               
+
+                _db.SaveChanges();
+
+                return new HttpResponseMessage(HttpStatusCode.OK);
+
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+        }
+
+    }
+}
---
+++ ASP.NET WEB API MVC/CarteleraApi/CarteleraApi/Controllers/PeliculasApiController.cs
@@ -0,0 +1,104 @@
+﻿using CarteleraApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CarteleraApi.Controllers
+{
+    public class PeliculasApiController : ApiController
+    {
+
+        private CarteleraEntities3 _db = new CarteleraEntities3();
+
+
+
+
+        //public HttpResponseMessage Get(int id)
+        //{
+
+        //    var pelicula = _db.Peliculas.FirstOrDefault(x => x.id == id);
+
+        //    return Request.CreateResponse<Peliculas>(HttpStatusCode.OK, pelicula);
+        //}
+
+  
+
+        public HttpResponseMessage Get()
+        {
+            var peliculas = _db.Peliculas;
+
+            return Request.CreateResponse<IEnumerable<Peliculas>>(HttpStatusCode.OK, peliculas);
+        }
+
+        public HttpResponseMessage Get(int? id)
+        {
+            var pelicula = _db.Peliculas.FirstOrDefault(x => x.id == id);
+            return Request.CreateResponse<Peliculas>(HttpStatusCode.OK, pelicula);
+        }
+
+        public HttpResponseMessage Post([FromBody]Peliculas newpeli)
+        {
+
+            _db.Peliculas.Add(newpeli);
+            _db.SaveChanges();
+
+            return new HttpResponseMessage(HttpStatusCode.Created);
+        }
+
+
+        public HttpResponseMessage Delete(int id)
+        {
+
+            var peliactor = _db.PeliculaActores.Where(x => x.idpelicula == id);
+            var deletepelicula = _db.Peliculas.FirstOrDefault(x => x.id == id);
+
+            if (peliactor != null)
+            {
+                _db.PeliculaActores.RemoveRange(peliactor);
+                _db.SaveChanges();
+            }
+
+            
+            if (deletepelicula != null)
+            {
+                _db.Peliculas.Remove(deletepelicula);
+                _db.SaveChanges();
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+        }
+
+        public HttpResponseMessage Put([FromBody]Peliculas oldpeli)
+        {
+
+            var putpelicula = _db.Peliculas.FirstOrDefault(x => x.id == oldpeli.id);
+
+            if (putpelicula != null)
+            {
+                putpelicula.titulo = oldpeli.titulo;
+                putpelicula.genero = oldpeli.genero;
+                putpelicula.ano = oldpeli.ano;
+                putpelicula.sinopsis = oldpeli.sinopsis;
+
+                _db.SaveChanges();
+
+                return new HttpResponseMessage(HttpStatusCode.OK);
+
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+        }
+
+
+    }
+}
